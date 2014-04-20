2cbbf220866e8c962cb525623ddff41d40a2bcff
andidegn <andi_degn@yahoo.com>
2014-04-21 00:29:14 +0200

    test

--- src/mob/assignment/mediaplayerassignment/MainActivity.java
+++ src/mob/assignment/mediaplayerassignment/MainActivity.java
@@ -5,16 +5,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
--- src/mob/assignment/mediaplayerassignment/MediaPlayerServices.java
+++ src/mob/assignment/mediaplayerassignment/MediaPlayerServices.java
@@ -8,72 +8,72 @@ import android.os.IBinder;
 import android.os.Binder;
 
 public class MediaPlayerServices extends Service {
-	
-	 private final IBinder mBinder = new LocalBinder();
-	    private MediaPlayer mp;
 
-	    public MediaPlayerServices() {
-	    }
+	private final IBinder mBinder = new LocalBinder();
+	private MediaPlayer mp;
 
-	    public class LocalBinder extends Binder {
-	    	MediaPlayerServices getService() {
-	           
-	            return MediaPlayerServices.this;
-	        }
-	    }
-	    
-	    
+	public MediaPlayerServices() {
+	}
+
+	public class LocalBinder extends Binder {
+		MediaPlayerServices getService() {
+
+			return MediaPlayerServices.this;
+		}
+	}
+
 	@Override
 	public IBinder onBind(Intent intent) {
-		 mp = mp.create(this, Uri.parse("http://gototen.dk/wp-content/uploads/2013/12/dont-mess-with-my-man.mp3"));
-	        return mBinder;
+		mp = mp.create(
+				this,
+				Uri.parse("http://gototen.dk/wp-content/uploads/2013/12/dont-mess-with-my-man.mp3"));
+		return mBinder;
 	}
-	 public void play() {
-	        if(mp == null) {
-	            return;
-	        }
-	        if(!mp.isPlaying()) {
-	//        	 mp = mp.create(this, Uri.parse("http://gototen.dk/wp-content/uploads/2013/12/dont-mess-with-my-man.mp3"));
-	            mp.start();
-	            
-	        }
-	    }
 
-	    public void pause() {
-	    	if (mp == null) {
-				return;
-			}
-			if (!mp.isPlaying()) {
-				mp.start();
+	public void play() {
+		if (mp == null) {
+			return;
+		}
+		if (!mp.isPlaying()) {
+			// mp = mp.create(this,
+			// Uri.parse("http://gototen.dk/wp-content/uploads/2013/12/dont-mess-with-my-man.mp3"));
+			mp.start();
 
-			} else {
-				mp.pause();
-			}
-	    }
+		}
+	}
 
-	    public void stop() {
-	    	if (mp == null) {
-				return;
-			}
-			if (mp.isPlaying()) {
-				mp.pause();
-			}
-			mp.seekTo(0);
-		
-	    }
-	    
-	    public void reset(){
-	    mp.reset();	
-	    	
-	    }
+	public void pause() {
+		if (mp == null) {
+			return;
+		}
+		if (!mp.isPlaying()) {
+			mp.start();
 
-	    public void playSong(String songPath) {
-	        mp.reset();
-	        mp = MediaPlayer.create(this, Uri.parse(songPath));
-	        mp.start();
-	    }
+		} else {
+			mp.pause();
+		}
+	}
 
+	public void stop() {
+		if (mp == null) {
+			return;
+		}
+		if (mp.isPlaying()) {
+			mp.pause();
+		}
+		mp.seekTo(0);
 
 	}
 
+	public void reset() {
+		mp.reset();
 
+	}
+
+	public void playSong(String songPath) {
+		mp.reset();
+		mp = MediaPlayer.create(this, Uri.parse(songPath));
+		mp.start();
+	}
+
+}
