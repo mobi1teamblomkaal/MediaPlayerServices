8f51b74eba310c371cb04ab976596b7b1bd3610a
andidegn <andi_degn@yahoo.com>
2014-04-30 10:54:09 +0200

    Updated Channel class to include an episode list and belonging methods

--- src/mob/assignment/rss/domain/model/Channel.java
+++ src/mob/assignment/rss/domain/model/Channel.java
@@ -1,17 +1,21 @@
 package mob.assignment.rss.domain.model;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Channel {
 	private String name;
 	private String description;
 	private URL coverart;
 	
+	private ArrayList<Episode> episodeList;
+	
 	public Channel() {
-		
+		episodeList = new ArrayList<Episode>();
 	}
 	
 	public Channel(String name, String description, URL coverart) {
+		this();
 		this.name = name;
 		this.description = description;
 		this.coverart = coverart;
@@ -39,5 +43,20 @@ public class Channel {
 
 	public void setCoverart(URL coverart) {
 		this.coverart = coverart;
+	}
+	
+	public void addEpisode(Episode e) {
+		episodeList.add(e);
+	}
+	
+	public Episode getEpisode(int index) throws IndexOutOfBoundsException {
+		if (index > episodeList.size() - 1) {
+			throw new IndexOutOfBoundsException();
+		}
+		return episodeList.get(index);
+	}
+	
+	public ArrayList<Episode> getEpisodeList() {
+		return episodeList;
 	}
 }
